781c3dcf1cb4505097d85a502cc0413c34179abe
JefChen <45091435+Chen-Jef@users.noreply.github.com>
2022-12-03 13:51:17 +0800

    del
    1

--- jef/dcat-config/src/Http/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Dcat\Admin\Jef\DcatConfig\Http\Controllers;
-use Illuminate\Support\Facades\Route;
-
-Route::get('jef/dcat-config', Controllers\DcatConfigController::class.'@index');
-Route::any('jef/dcat-config', Controllers\DcatConfigController::class.'@index');
